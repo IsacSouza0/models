e6e0472b9b4d75043e1a6ce1c13f3f8514aa226e
Beatriz <biiah_beatriz@hotmail.com>
2019-02-27 12:17:38 -0300

    Aula moldels

---
+++ vingadores/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from vingadores.models import Banda, Integrantes, Album
+
+# Register your models here.
+
+admin.site.register(Banda)
+admin.site.register(Integrantes)
+admin.site.register(Album)
